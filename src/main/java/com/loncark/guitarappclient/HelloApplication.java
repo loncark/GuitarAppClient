3992298a7ac0a6db8a13dd685d8296673818c54d
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2023-06-04 18:28:48 +0200

    Added elements of xml to client

--- src/main/java/com/loncark/guitarappclient/HelloApplication.java
+++ src/main/java/com/loncark/guitarappclient/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("GuitarClient.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("GuitarApp");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/loncark/guitarappclient/HelloController.java
+++ src/main/java/com/loncark/guitarappclient/HelloController.java
@@ -1,14 +1,58 @@
 package com.loncark.guitarappclient;
 
+import com.loncark.guitarappclient.module.Guitar;
+import com.loncark.guitarappclient.module.Material;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    TextField codeFieldGET;
+    @FXML
+    Button getByCodeButton;
+    @FXML
+    Button getAllButton;
+    @FXML
+    TableView<Guitar> tableGET;
 
+    @FXML
+    TextField codeFieldPOST;
+    @FXML
+    TextField nameFieldPOST;
+    @FXML
+    TextField priceFieldPOST;
+    @FXML
+    ComboBox<Material> neckComboPOST;
+    @FXML
+    ComboBox<Material> bodyComboPOST;
+    @FXML
+    ComboBox<Long> stockComboPOST;
+    @FXML
+    ComboBox<Long> idComboPOST;
+    @FXML
+    Button submitButtonPOST;
+    @FXML
+    Label messagePOST;
+
+    @FXML
+    ComboBox<String> codeComboDELETE;
+    @FXML
+    Label messageDELETE;
+    @FXML
+    Button deleteButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
+
+
 }
